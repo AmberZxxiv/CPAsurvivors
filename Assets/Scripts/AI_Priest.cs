3f0629c2c7893ab1d47eb099f0882f99bc583d02
AmberZxxiv <amberzxxiv@gmail.com>
2025-05-28 19:13:26 +0200

    TENEMOS JUEGO
    Falta arreglar monedas, rutas random y balancear enemigos

--- Assets/Scripts/AI_Priest.cs
+++ Assets/Scripts/AI_Priest.cs
@@ -5,31 +5,52 @@ using UnityEngine.AI;
 
 public class AI_PRiest : MonoBehaviour
 {
+    // le damos a la IA los objetivos varios
     public Transform objective;
     public int speed;
     public NavMeshAgent AI;
-    private bool isFollowing = false;
+    private bool isFollowing;
+    public Transform[] patrolPoints;
+    public int targetPoint;
 
     // Start is called before the first frame update
     void Start()
     {
+        // que empiece a hacer sus cosas
+        isFollowing = false;
         AI.speed = speed;
+        targetPoint = 0;
+        AI.SetDestination(patrolPoints[targetPoint].position);
     }
 
     // Update is called once per frame
     void Update()
     {
+        // si no me sigue, que haga su ruta
+        if (!isFollowing)
+        {
+            AI.SetDestination(patrolPoints[targetPoint].position);
+
+            // si está suficientemente cerca del checkpoint, que cambie
+            if (!AI.pathPending && AI.remainingDistance < 0.5f)
+            {
+                print("CHECK point " + targetPoint);
+                NextTarget();
+            }
+        }
+
+        // si me sigue, que vaya a por mi
         if (isFollowing)
         {
             AI.SetDestination(objective.position);
         }
     }
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             isFollowing = true;
+            print("FOLLOW");
         }
     }
 
@@ -38,6 +59,15 @@ public class AI_PRiest : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             isFollowing = false;
+            print("NOT follow");
         }
+    }
+
+    void NextTarget()
+    {
+        // que sume 1 a la lista y vaya pa alla
+        targetPoint = (targetPoint + 1) % patrolPoints.Length;
+        print("NEW target " + targetPoint);
+        AI.SetDestination(patrolPoints[targetPoint].position);
     }
 }
